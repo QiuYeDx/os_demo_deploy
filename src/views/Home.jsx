be45ee059e55a759033da7c87903206bcd1013fc
白子涵(秋夜Dx <71062076+QiuYeDx@users.noreply.github.com>
2023-03-23 11:28:38 +0800

    update README.md

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -21,7 +21,7 @@ export default class Home extends React.Component {
                         <H1>操作系统实验<br/>
                             实验一演示</H1>
                         <Gap/>
-                        <Text>1.设计并实现一个模拟进程状态转换及其 相应PCB组织结构变化的程序;<br/>
+                        <Text>1.设计并实现一个模拟进程状态转换及其相应PCB组织结构变化的程序;<br/>
                             2.独立设计、编写、调试程序;<br/>
                             3.程序界面应能反映出在模拟条件下，进程之间状态转换及其对应的PCB组织的变化。<br/>
                         </Text>
